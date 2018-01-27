845ad61c01351b15978fc43de25996b94bb6b9d8
Jesus <jesus@jesuss-mac.local>
2018-01-27 22:59:08 +0100

    Initial Commit

---
+++ MetroSQLiteSwiftTests/MetroSQLiteSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  MetroSQLiteSwiftTests.swift
+//  MetroSQLiteSwiftTests
+//
+//  Created by Jesus on 27/1/18.
+//  Copyright © 2018 Jesus. All rights reserved.
+//
+
+import XCTest
+@testable import MetroSQLiteSwift
+
+class MetroSQLiteSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
